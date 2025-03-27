ff52def73d96f9bd43c63ae9ac9f65191ad58789
Adam Cygan <adam.cygan@onet.eu>
2025-03-27 13:11:46 +0100

    feat: added data-fetching module with add, update and delete methods

---
+++ src/context/ReservationContext.tsx
@@ -0,0 +1,100 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState
+} from 'react';
+import { Reservation } from '../types/reservation';
+import reservationsData from '../data/reservations.json';
+import { mapResponseObjectToReservation } from '../utils/reservationUtils';
+
+interface ReservationContextProps {
+  reservations: Reservation[];
+  setReservations: (reservations: Reservation[]) => void;
+  addReservation: (reservation: Reservation) => void;
+  updateReservation: (reservation: Reservation) => void;
+  deleteReservation: (reservationId: string) => void;
+  generateReservationId: () => string;
+}
+
+const ReservationContext = createContext<ReservationContextProps | undefined>(
+  undefined
+);
+
+export const ReservationProvider = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  const [reservations, setReservations] = useState<Reservation[]>(
+    reservationsData.map(mapResponseObjectToReservation)
+  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        const validReservations = reservationsData.map(
+          mapResponseObjectToReservation
+        );
+        setReservations(validReservations);
+      } catch (error) {
+        console.error('Błąd podczas przetwarzania danych rezerwacji:', error);
+      } finally {
+        setLoading(false);
+      }
+    }, 800);
+  }, []);
+
+  const addReservation = useCallback((reservation: Reservation) => {
+    setReservations((prev) => [...prev, reservation]);
+  }, []);
+
+  const updateReservation = useCallback((reservation: Reservation) => {
+    setReservations((prev) =>
+      prev.map((res) => (res.id === reservation.id ? reservation : res))
+    );
+  }, []);
+
+  const deleteReservation = useCallback((id: string) => {
+    setReservations((prev) => prev.filter((res) => res.id !== id));
+  }, []);
+
+  const generateReservationId = useCallback(() => {
+    if (reservations.length === 0) return 'res-001';
+
+    const lastId = reservations[reservations.length - 1].id;
+    const idNumber = parseInt(lastId.split('-')[1]);
+    const nextId = (idNumber + 1).toString().padStart(3, '0');
+    return `res-${nextId}`;
+  }, [reservations]);
+
+  return (
+    <ReservationContext.Provider
+      value={{
+        reservations,
+        setReservations,
+        addReservation,
+        updateReservation,
+        deleteReservation,
+        generateReservationId
+      }}
+    >
+      {loading ? (
+        <div className='loading'>Ładowanie danych rezerwacji...</div>
+      ) : (
+        children
+      )}
+    </ReservationContext.Provider>
+  );
+};
+
+export const useReservationContext = () => {
+  const context = useContext(ReservationContext);
+  if (!context)
+    throw new Error(
+      'useReservationContext must be used inside ReservationProvider'
+    );
+  return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router';
 import { Toaster } from 'sonner';
 import AppRoutes from './routes/AppRoutes.tsx';
+import { ReservationProvider } from './context/ReservationContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AppRoutes />
-      <Toaster position='bottom-right' />
+      <ReservationProvider>
+        <AppRoutes />
+        <Toaster position='bottom-right' />
+      </ReservationProvider>
     </BrowserRouter>
   </StrictMode>
 );
